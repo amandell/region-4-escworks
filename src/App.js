b8338c397d594df7343a734d12c0abce2bb72e6d
Alec Mandell <amandell94@gmail.com>
2023-11-15 14:07:03 -0600

    api gateway call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+
+const url = 'https://twp43i3jd6.execute-api.us-east-2.amazonaws.com/region-4-escworks/getData'
 
 function App() {
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    fetch(url, {mode: 'no-cors'})
+      .then(res => console.log(res));
+  }
+  
   return (
     <div className="App">
       <h1>Hello World</h1>
